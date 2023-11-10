dd7747af158d7361e2c2f4fa40c08fced9894037
jackgusler <guslerj1@newpaltz.edu>
2023-11-10 15:39:44 -0500

    worked on the homepage, changed name to client

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import LoginBadge from "./LoginBadge.vue";
+//import { getSession } from "@/models/session";
+import { ref } from "vue";
+
+//const session = getSession();
+const isActive = ref(false);
+</script>
+
+<template>
+    <div>
+        <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <RouterLink class="navbar-item" to="/">
+              <img
+                src="https://bulma.io/images/bulma-logo-white.png"
+                width="112"
+                height="28"
+              />
+            </RouterLink>
+            <a
+              role="button"
+              class="navbar-burger"
+              :class="{ 'is-active': isActive }"
+              @click="isActive = !isActive"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div
+            class="navbar-menu"
+            id="navbarMenu"
+            :class="{ 'is-active': isActive }"
+          >
+            <div class="navbar-start">
+              <RouterLink class="navbar-item" to="/my-workouts"
+                >My Workouts</RouterLink
+              >
+              <RouterLink class="navbar-item" to="/statistics"
+                >Stats</RouterLink
+              >
+              <RouterLink class="navbar-item" to="/friends">Friends</RouterLink>
+            </div>
+            <LoginBadge />
+          </div>
+        </nav>
+      </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const rowValue = ref(0)
+const columnValue = ref(0)
+const isActive = ref(false)
+
+function openModal() {
+  isActive.value = true
+} 
+</script>
+
+<template>
+  <div>
+    <div class="modal" :class="{'is-active': isActive}">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+            <div class="field">
+                <label class="label">Category</label>
+                <div class="control">
+                <input class="input" type="text">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Budget</label>
+                <div class="control">
+                <input class="input" type="text">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Spent</label>
+                <div class="control">
+                <input class="input" type="text">
+                </div>
+            </div>
+            <div class="buttons">
+                <div class="button is-primary" @click="(isActive = false) && (rowValue++)">Save changes</div>
+                <div class="button" @click="isActive = false">Cancel</div>
+            </div>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="isActive = false"></button>
+    </div>
+    <div class="panel">
+      <div class="panel-heading">
+        <h3 class="panel-title">Budget</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Categories</th>
+              <th>Budget</th>
+              <th>Spent</th>
+            </tr>
+          </thead>
+          <tbody v-for="i in rowValue" :key="i">
+            <tr>
+              <td>{{ i }}</td>
+              <td>$</td>
+              <td>$</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Total</td>
+              <td>$</td>
+              <td>$</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="buttons">
+      <div class="button" @click="isActive = true">Row+</div>
+      <div class="button" @click="rowValue--">Row-</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.panel-title {
+  text-align: center;
+}
+.buttons {
+  display: flex;
+  justify-content: center;
+}
+</style>
